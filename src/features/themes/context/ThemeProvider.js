6bf496cd300121fbefc54ca7b1833b3c90847e08
Kenni Rodriguez <ken.rodriguez@estudiantec.cr>
2026-04-22 13:39:45 -0600

    feature(theme): new feature, themes.

---
+++ src/features/themes/context/ThemeProvider.js
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import { ThemeContext } from './ThemeContext';
+
+export function ThemeProvider({children}) {
+    const [theme, setTheme] = useState("light")
+    const toggleTheme = () => {
+        setTheme((prev) => (prev == "light" ? "dark" : "light")) 
+    }
+
+}
